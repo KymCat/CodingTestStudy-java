fa9d5a32901f6516be57b221cdad35d0c22d34db
KymCat <kohina1452@gmail.com>
2025-12-12 19:58:32 +0900

    [백준] 11003번 최솟값 찾기 문제

---
+++ 문제/골드/G11003.java
@@ -0,0 +1,45 @@
+package 골드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class G11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            while (!queue.isEmpty() && queue.getLast().value > cur)
+                queue.removeLast();
+
+            queue.addLast(new Node(i,cur));
+            if(i - l >= queue.getFirst().index)
+                queue.removeFirst();
+
+            sb.append(queue.getFirst().value).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static class Node {
+        private int index;
+        private int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
+}
